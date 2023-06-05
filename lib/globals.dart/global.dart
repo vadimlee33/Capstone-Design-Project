b629ea9b6dc2abe34d135f11e9d5ca961800c9e7
vadimlee33 <vadimthegreat3@gmail.com>
2023-06-05 18:21:09 +0900

    songs

--- lib/globals.dart/global.dart
+++ lib/globals.dart/global.dart
@@ -22,6 +22,84 @@ final emotionColors = {
   'surprise': Color(0xFFE4C1F9)
 };
 
+const List<String> sadnessSongsList = [
+  'Alec Benjamin - If We Have Each Other',
+  'Duncan Laurence - Arcade',
+  'Måneskin - THE LONELIEST',
+  'Imagine Dragons - Wrecked',
+  'Hillsong United - Ocean',
+  'Avril Lavigne - When you’re gone',
+  'Betperde - MEREY',
+  'Want it? - Zemfira',
+  'Evanescence - My Immortal',
+  'Markul - Fata Morgana'
+];
+
+const List<String> joySongsList = [
+  'King Harvest - Dancing in The Moonlight',
+  'Corinne Bailey Rae - Put Your Records On',
+  'Post Malone - Sunflower',
+  'Alphaville - Forever young',
+  'Vance Joy - Riptide',
+  'Nick Youre, dazy - Sunroof',
+  'Dem - Raim',
+  'Griby - Taet led',
+  'Lil Skies - Nowadays',
+  'Neffex - Best of Me'
+];
+
+const List<String> loveSongsList = [
+  'Tyler, The Creator - ARE WE STILL FRIENDS?',
+  'slchld, whoosh - still friends, not lovers',
+  'Stephen Sanchez - Until I found you',
+  'Harry Styles - Love of My life',
+  'Taylor Swift - Love story',
+  'New West - Those eyes',
+  'Joji - Glimpse of Us',
+  'Kina, Snow - Get You The Moon',
+  'TWICE - What is Love',
+  'Halsey - Bad At Love'
+];
+
+const List<String> angerSongsList = [
+  'Tyler, The Creator - SORRY NOT SORRY',
+  'Nirvana - Smells Like Teen Spirit',
+  'Imagine Dragons - Dull Knives',
+  'Oasis - Don’t look back in anger',
+  'Imagine Dragons - Bad Liar',
+  'Imagine Dragons- Natural',
+  'Coolie, L.V. - Gangsta’s Paradise',
+  'Ghostmane - Mercury:Retrograde',
+  'Evanescence - Bring Me To Life',
+  'Eminem - Lose Yourself'
+];
+
+const List<String> fearSongsList = [
+  'Anson Seabra - Trying My Best',
+  'Alley Duhé - Middle of the night',
+  'Broken People - almost monday',
+  'Vampire - Dominic Fike',
+  'Jeremy Zucker - Scared',
+  'NF - Let You Down',
+  'XXXtentacion - SAD!',
+  'Cigarettes after sex - Apocalypse',
+  'Han Yo Han - 400km',
+  'Lil Happy Lil Sad - Survive'
+];
+
+const List<String> surprisedSongsList = [
+  'Bruno Mars - Treasure',
+  'Mac Miller - GOOD NEWS',
+  'Post Malone - Wow',
+  'Alex Benjamin - Oh My God',
+  'The Script - Hall of Fame',
+  'Imagine Dragons - It’s time',
+  'Dybbukk, Sabrina Gomes - It’s Not So Bad',
+  '1nonly - Stay With Me',
+  'LiSA - Unlasting',
+  'Machine Gun Kelly - my ex’s best friend'
+];
+
 Map<String, dynamic> diaryEntries = {};
 
 Map<String, dynamic> diaryEntriesTest = {};
